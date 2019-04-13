09a971c07e2fc5f4c2b3dfe08ed04452b5c50da9
TinaHongBu <butianhong@outlook.com>
2019-04-12 22:11:36 -0700

    created package folder structure

---
+++ ding/__init__.py
@@ -0,0 +1,4 @@
+from ding import Alarm
+
+alarm = Alarm()
+alarm.ring_alarm()
---
+++ ding/alarm.py
@@ -0,0 +1,12 @@
+class Alarm:
+	def __init__(self):
+		'''
+		Constructor
+		'''
+		self.alarm_audio = 
+		
+	def ring_alarm(self):
+		'''
+		play an alarm sound
+		'''
+		
