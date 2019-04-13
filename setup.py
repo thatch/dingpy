c2bf15a44c9c92c8145857360a7386b51cad30f0
TinaHongBu <butianhong@outlook.com>
2019-04-12 22:23:37 -0700

    added audio files and tested playing from terminal

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Plays alarm when script finishes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Tina-Bu/ding/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
